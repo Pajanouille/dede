adeb786f095eab0eb3f599eaebe0a56c6ea068fb
pajanouille <pajany78@gmail.com>
2022-07-27 09:39:42 +0400

    fini

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,24 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(DedeApp());
+}
+
+class DedeApp extends StatefulWidget {
+  const DedeApp({Key? key}) : super(key: key);
+
+  @override
+  State<DedeApp> createState() => _DedeAppState();
+}
+
+class _DedeAppState extends State<DedeApp> {
+  int des1 = Random().nextInt(6) + 1;
+  int des2 = Random().nextInt(6) + 1;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
@@ -18,19 +34,33 @@ void main() {
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child: Image.asset('assets/images/dice1.png'),
+                  child: MaterialButton(
+                      onPressed: () {
+                        setState(() {
+                          des1 = Random().nextInt(6) + 1;
+                          des2 = Random().nextInt(6) + 1;
+                        });
+                      },
+                      child: Image.asset('assets/images/dice$des1.png')),
                 ),
               ),
               Expanded(
                 child: Padding(
                   padding: const EdgeInsets.all(16.0),
-                  child: Image.asset('assets/images/dice2.png'),
+                  child: MaterialButton(
+                      onPressed: () {
+                        setState(() {
+                          des1 = Random().nextInt(6) + 1;
+                          des2 = Random().nextInt(6) + 1;
+                        });
+                      },
+                      child: Image.asset('assets/images/dice$des2.png')),
                 ),
               ),
             ],
           ),
         ),
       ),
-    ),
-  );
+    );
+  }
 }
